09afb98107f59edf4e76f1c3007639e826fac979
Pranav Gupta <pranavgupta194@gmail.com>
2020-04-14 11:46:08 +0530

    planet added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import RangeSlider from 'react-bootstrap-range-slider';
 import LineChart from 'react-linechart';
 import '../node_modules/react-linechart/dist/styles.css';
+import Select from 'react-select';
 
 
 
@@ -27,13 +28,12 @@ const getdata=(height,time,e,g,timegap)=>{
   datas.push({x:timetofall1+time,y:0})
   const timetofall2 = e*timetofall1;
   let datas2 = [];
-  for(let i = 0;i<=timetofall2;i+=timegap)
+  for(let i = 0;i<timetofall2;i+=timegap)
   {
     let h = e*e*height - 1/2*g*i*i;
     datas2 = [{x:timetofall2+timetofall1-i+time,y:h},...datas2]
   }
-  console.log(e+"coming here");
-  datas2 =[...datas2,{x:timetofall1+timetofall2+time,y:parseInt(e*e*height)}];
+  // datas2 =[...datas2,{x:timetofall1+timetofall2+time,y:parseInt(e*e*height)}];
 
   const newTime = timetofall2+timetofall1+time;
   const newHeight = e*e*height;
@@ -46,14 +46,13 @@ const getdata=(height,time,e,g,timegap)=>{
 
 
 function Header(){
-  return (<h2>Edunomics React Graph</h2>)
+  return (<h1>Edunomics React Graph</h1>)
 }
 
 function Slider({onChange})
 {
   const [value,setValue] = useState(0.4);
   const onSlide=(e)=>{
-    // console.log(e.target.value);
     setValue(e.target.value);
     onChange(e.target.value);
   }
@@ -62,7 +61,6 @@ function Slider({onChange})
     <h4>Coefficient of Restitution:-  {value}</h4>
     <RangeSlider
       step ={0.01}
-      tooltipPlacement ={'top'}
         min = {0}
         max = {0.99}
         value={+value}
@@ -73,12 +71,12 @@ function Slider({onChange})
   )
 }
 function Graph(){
-  // const [data1,setData] = useState([]);
+  const [g,setG] = useState(9.8);
   
-  const g = 9.8;
+  // const g = 9.8;
   const [height,setHeight] = useState(50);
   const [ret,setRet] = useState(0.4);
-  let timegap = 0.5;
+  let timegap = Math.min(0.5,height/100);
   let data1=getdata(height,0,ret,g,timegap);
   
   const onChange = (e)=>{
@@ -87,12 +85,16 @@ function Graph(){
     data1 = getdata(height,0,e,g,timegap);
   }
   const onSubmit = (e)=>{
-    e.preventDefault();
-    console.log(ret);
-   
+    e.preventDefault();   
     data1 = getdata(height,0,ret,g,timegap);
   }
-   
+  const options = [
+    { value: 3.7, label: 'Mercury' },
+    { value: 9.8, label: 'Earth' },
+    { value: 3.71, label: 'Mars' },
+    { value: 24.80, label: 'Jupiter' },
+    { value: 10.44, label: 'Saturn' },
+  ]
   let data = [
     {									
         color: "steelblue", 
@@ -105,8 +107,15 @@ function Graph(){
       <Slider onChange = {onChange}/>
       <br/>
       <form className = "my-form" onSubmit = {onSubmit}>
-        <label>Height <strong> (Please enter positive number less than 5000)</strong></label>
-        <input type = "number" min="1" max="50000" value ={height} onChange = {e=>setHeight(e.target.value)}/>     
+        <div  className = "form-group">
+          <label><strong>Height</strong> <span className = "info">(Please enter positive number less than 5000)</span></label>
+          <input type = "number" min="1" max="50000" value ={height} onChange = {e=>setHeight(e.target.value)}/>   
+        </div>
+        <div  className = "form-group">
+          <label>Planet</label>
+          <Select className="basic-single"
+          classNamePrefix="select" onChange={(e)=>setG(e.value)} defaultValue={options[1]} options={options} />
+        </div>
         <button className ="btn">Make Graph</button>
       </form>
       
@@ -126,6 +135,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <br/>
       <div className = "container">
         
         <Graph/>
